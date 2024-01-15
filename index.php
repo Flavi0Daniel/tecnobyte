ed51703f4a520c55b719b5365defe8ac3717cd5a
Edmilson Quiala <edmilson.quiala@amsp.ao>
2024-01-15 14:34:45 +0100

    Alterações index.php
    alterou-se o logo
    alterou-se o ícone
    alterou-se a listagem

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Tecnobyte - Página Inicial</title>
+    <!-- shortcut icon & favicon-->
+    <link rel="shortcut icon" href="logo-fundo.png" type="image/png">
+    <link rel="icon" href="logo-fundo.png" type="image/png">
+
 </head>
+
 <body>
     <header>
         <h1>Tecnobyte</h1>
@@ -20,39 +27,46 @@
     </header>
 
     <section id="destaques">
-       
-    
 
-<h2>Produtos em Destaque</h2>
+        <h2>Produtos em Destaque</h2>
 
-<div class="produto">
-    <img src="baixados.png" alt="Produto em Destaque 1">
-    <p>ASUS F415</p>
-    <p>Preço: 550.000,00 KZ</p>
-</div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="produto">
+                        <img src="baixados.png" alt="Produto em Destaque 1">
+                        <p>ASUS F415</p>
+                        <p>Preço: 550.000,00 KZ</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="produto">
+                        <img src="719aLEkzkGL._AC_SL1280_-removebg-preview.png" alt="Produto em Destaque 2">
+                        <p>Mpow H7 Auricular Bluetooh</p>
+                        <p>Preço: 17.000,00 KZ</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="produto">
+                        <img src="mouse-sem-fio-logitech-m187-24ghz-azul-50105-2000-201470-removebg-preview.png" alt="Produto em Destaque 3">
+                        <p>Mouse sem FIO Logitech M187 2.4ghz</p>
+                        <p>Preço: 6.499,99 KZ</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h2>Breve Descrição da Tecnobyte</h2>
 
-<div class="produto">
-    <img src="719aLEkzkGL._AC_SL1280_-removebg-preview.png" alt="Produto em Destaque 2">
-    <p>Mpow H7 Auricular Bluetooh</p>
-    <p>Preço: 17.000,00 KZ</p>
-</div>
-
-<div class="produto">
-    <img src="mouse-sem-fio-logitech-m187-24ghz-azul-50105-2000-201470-removebg-preview.png" alt="Produto em Destaque 3">
-    <p>Mouse sem FIO Logitech M187 2.4ghz</p>
-    <p>Preço: 6.499,99 KZ</p>
-</div>
-
-
-
-<h2>Breve Descrição da Tecnobyte</h2>
-
-<p>Bem-vindo à Tecnobyte, sua loja online de tecnologia! Oferecemos uma ampla gama de produtos eletrônicos de alta qualidade, desde smartphones e laptops até acessórios inovadores. Nossa missão é proporcionar a você as últimas e melhores tecnologias a preços acessíveis. Explore nosso catálogo e descubra as soluções tecnológicas que você procura.</p>
+        <p>Bem-vindo à Tecnobyte, sua loja online de tecnologia! Oferecemos uma ampla gama de produtos eletrônicos de alta qualidade, desde smartphones e laptops até acessórios inovadores. Nossa missão é proporcionar a você as últimas e melhores tecnologias a preços acessíveis. Explore nosso catálogo e descubra as soluções tecnológicas que você procura.</p>
 
     </section>
 
     <footer>
         <p>&copy; 2023 Tecnobyte. Todos os direitos reservados.</p>
     </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+</body>
+
 </html>
